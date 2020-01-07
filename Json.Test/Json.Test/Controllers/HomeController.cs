0ba999ee9bfb6304b49a16725421e35129d60344
Vincenzo <ten@disroot.org>
2020-01-07 14:09:35 +0100

    First Draft

---
+++ Json.Test/Json.Test/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Json.Test.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Json.Test.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public OkObjectResult Submit(ViewModel data)
+		{
+			return Ok(data);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+
+	public class ViewModel
+	{
+		public string Field1 { get; set; }
+		public decimal Field2 { get; set; }
+
+	}
+}
